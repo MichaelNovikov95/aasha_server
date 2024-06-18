5f3ed5e371c2460ed36244cf69011e11320f1512
Mykhailo Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2024-06-18 16:42:46 +0300

    fix stripe payment

--- src/controllers/paymentController.mjs
+++ src/controllers/paymentController.mjs
@@ -5,17 +5,27 @@ dotenv.config();
 const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 export const paymentHandler = async (req, res) => {
-  const { items } = req.body;
-  console.log(items);
+  const items = req.body;
   try {
-    // const session = await stripe.checkout.session.create({
-    // payment_method_types: ["card"],
-    // mode: "payment",
-    // line_items: items,
-    // success_url: `${process.env.SERVER_URL}/success`,
-    // cancel_url: `${process.env.SERVER_URL}/cancel`,
-    // });
-    // res.send({ url: session.url });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: items.map((item) => {
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.name,
+            },
+            unit_amount: (item.totalPrice / item.count) * 100,
+          },
+          quantity: item.count,
+        };
+      }),
+      success_url: `${process.env.SERVER_URL}/success`,
+      cancel_url: `${process.env.SERVER_URL}/cancel`,
+    });
+    res.send({ url: session.url });
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
