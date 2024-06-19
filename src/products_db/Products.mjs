07f0045de24173c905213df90433beb57f747f7a
Mykhailo Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2024-06-19 17:28:34 +0300

    test lazy load image

--- src/products_db/Products.mjs
+++ src/products_db/Products.mjs
@@ -29,6 +29,8 @@ const Products = [
       "One size only: 4,5cm",
     ],
     image_src: "https://aasha-server.onrender.com/images/Pins/pin_2.jpg",
+    small_image_src:
+      "https://aasha-server.onrender.com/images/Pins/pin_2_small.jpg",
   },
   {
     id: 3,
@@ -42,6 +44,8 @@ const Products = [
       "One size only: 4,5cm",
     ],
     image_src: "https://aasha-server.onrender.com/images/Pins/pin_3.jpg",
+    small_image_src:
+      "https://aasha-server.onrender.com/images/Pins/pin_3_small.jpg",
   },
   {
     id: 4,
@@ -55,6 +59,8 @@ const Products = [
       "One size only: 4,5cm",
     ],
     image_src: "https://aasha-server.onrender.com/images/Pins/pin_4.jpg",
+    small_image_src:
+      "https://aasha-server.onrender.com/images/Pins/pin_4_small.jpg",
   },
   {
     id: 5,
